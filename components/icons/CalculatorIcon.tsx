f59af53a6e89ffcf489e6e3fbdc986e3fc532643
Sridhar Kulkarni <sridhar.kulkarni190@gmail.com>
2025-11-13 17:16:40 +0530

    Update site with latest build

--- components/icons/CalculatorIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const CalculatorIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-full h-full">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008Zm0 3h.008v.008H8.25v-.008Zm0 3h.008v.008H8.25v-.008Zm3-6h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm3-6h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008ZM6 12h12v6H6v-6Z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18h12a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2Z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9" />
-  </svg>
-);
